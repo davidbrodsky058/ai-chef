e2c67ef21fa5b305f31220944d364ed51b575697
shneorNudel <zalmi7700@gmail.com>
2025-05-27 12:23:58 +0300

    update eror

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Intro from './Intro'
 import Catalog from './Catalog'
-import Recipe from './Recipe'
-import './App.css'
+
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Intro />} />
             <Route path="/catalog" element={<Catalog />} />
-            <Route path="/recipe" element={<Recipe />} />
         </Routes>
     )
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from 'styled-components' 
-import { theme } from './theme'
-import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
